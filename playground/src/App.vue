5933ce1921d630e091683017b0dc3235a16c892a
凃锦东 <1965382331@qq.com>
2023-05-25 18:26:53 +0800

    chore: add publish sc

---
+++ playground/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="Icon" v-for="(Icon, key) in icons" :key="key" class="icon" />
+  <hr />
+  <component
+    :is="`TuIcon${key}`"
+    v-for="key in Object.keys(icons)"
+    :key="key"
+    class="icon"
+  />
+</template>
+
+<script lang="ts" setup>
+import { icons } from '@tu-view-plus/icons-vue/global'
+</script>
+
+<style>
+.icon {
+  height: 20px;
+  color: #409eff;
+}
+</style>
---
+++ playground/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import TuIcons from '@tu-view-plus/icons-vue/global'
+import App from './App.vue'
+
+createApp(App).use(TuIcons).mount('#app')
